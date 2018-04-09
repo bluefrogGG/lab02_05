b378e2210083e726b384a945c23c18d2fe86c772
Seoyoon Jang <lamian2@naver.com>
2018-04-09 12:05:10 +0900

    2017-12865

---
+++ lab2_05_2017_12865.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <GL/glut.h>
+#include <algorithm>
+
+using namespace std;
+
+void main() {
+	int num;
+	vector<int> numList;
+
+	while (std::cin >> num) {
+		numList.push_back(num);
+		
+	}
+	std::sort(numList.begin(), numList.end());
+	std::reverse(numList.begin(), numList.end());
+	
+	cout << "ordered array" << endl;
+	for (auto i : numList) {
+		cout << i << endl;
+	}
+}
